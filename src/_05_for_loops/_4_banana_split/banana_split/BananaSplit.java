fcfec1ebc2d39a6a842b38b768f1ae12d46a0d8a
Shail <sbrixey9@gmail.com>
2025-04-20 16:03:40 -0700

    4/20/25

---
+++ src/_05_for_loops/_4_banana_split/banana_split/BananaSplit.java
@@ -0,0 +1,15 @@
+package _05_for_loops._4_banana_split.banana_split;
+
+import javax.swing.JOptionPane;
+
+public class BananaSplit {
+  public static void main(String[] args) {
+    int IceCream = 0;
+    int IceCreamNum = 3;
+
+    for(IceCream = 0; IceCream <= IceCreamNum; IceCream+=1) {
+      System.out.println("ice cream");
+    }
+    System.out.print("banana");
+  }
+}
